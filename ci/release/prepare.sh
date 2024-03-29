5cb758279437a95a2b1f53c5c220f06283c9f1c5
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-03-29 06:59:53 -0400

    fix(build): wheels work

--- ci/release/prepare.sh
+++ ci/release/prepare.sh
@@ -8,4 +8,8 @@ version="${1}"
 nix develop '.#release' -c poetry version "$version"
 
 # build artifacts
-nix develop '.#release' -c poetry build --format=sdist
+nix develop '.#release' -c poetry build
+
+# ensure that the built wheel has the correct version number
+nix develop '.#release' -c unzip -p "dist/ibish-${version}-py3-none-any.whl" ibish/__init__.py | \
+  nix develop '.#release' -c grep -q "__version__ = \"$version\""
