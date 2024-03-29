79fa51a91bfce2988ebb1c03517fb9a207ee48f3
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-03-29 06:39:25 -0400

    chore: source release because who needs metadata

--- ci/release/prepare.sh
+++ ci/release/prepare.sh
@@ -7,9 +7,6 @@ version="${1}"
 # set version
 nix develop '.#release' -c poetry version "$version"
 
-# build artifacts
-nix develop '.#release' -c poetry build
-
 # ensure that the built wheel has the correct version number
 nix develop '.#release' -c unzip -p "dist/ibish-${version}-py3-none-any.whl" ibish/__init__.py | \
   nix develop '.#release' -c grep -q "__version__ = \"$version\""
