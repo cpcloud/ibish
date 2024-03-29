fd87a54c0397110c4c07b3a20980a0de9117c286
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-03-29 06:45:55 -0400

    chore: no wheel checking because no wheel

--- ci/release/prepare.sh
+++ ci/release/prepare.sh
@@ -9,7 +9,3 @@ nix develop '.#release' -c poetry version "$version"
 
 # build artifacts
 nix develop '.#release' -c poetry build --format=sdist
-
-# ensure that the built wheel has the correct version number
-nix develop '.#release' -c unzip -p "dist/ibish-${version}-py3-none-any.whl" ibish/__init__.py | \
-  nix develop '.#release' -c grep -q "__version__ = \"$version\""
